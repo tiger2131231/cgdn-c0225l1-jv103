e943d8522c0fd8ea244aa786905e94a090b62cc9
tiger2131231 <duongvuthai2018@gmail.com>
2025-05-28 19:03:06 +0700

    update bai1 ss6

---
+++ src/ss6_Class_Circle_Class_Cylinder/bai_tap/bai1/Circle.java
@@ -0,0 +1,41 @@
+package ss6_Class_Circle_Class_Cylinder.bai_tap.bai1;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public String toString() {
+        return "Circle[radius=" + radius + ", color=" + color + ", area=" + getArea() + "]";
+    }
+}
+
---
+++ src/ss6_Class_Circle_Class_Cylinder/bai_tap/bai1/TestCircle.java
@@ -0,0 +1,12 @@
+package ss6_Class_Circle_Class_Cylinder.bai_tap.bai1;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(2.5, "blue");
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}
+
